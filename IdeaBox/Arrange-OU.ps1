7b72f55b4df143f30aa483f79f119e131f8d9ba5
Nicholas Hoffmann <nihoffma@ab.bluecross.ca>
2018-12-19 11:24:08 -0700

    added

---
+++ IdeaBox/Arrange-OU.ps1
@@ -0,0 +1,66 @@
+﻿<#
+
+This function will retrieve the description of an AD computer then attempt to get an AD user out of that.
+
+With this user we can get the distnguished name which will correlate to the proper distinguished name of the computer
+
+When I get the Aho Corasick Algorighthm working I will get that included in here
+
+
+#>
+
+$ScriptPath = (Join-Path -Path $PSScriptRoot -ChildPath Get-StandardADAccounts.ps1) 
+$arguments = 'Office, Title'
+
+$Users = Invoke-Expression "$ScriptPath $arguments"
+$Users = $Users | Sort-Object SamAccountName
+
+
+
+$Computers = Get-ADComputer -Filter {OperatingSystem -like "*Windows 7*"} -Properties Description | ? {$_.Name -notlike "*VDI-*"} | ?{$_.DistinguishedName -notlike "*OU=Workstations*"}
+$Output = @()
+Foreach($computer in $Computers){
+    $Description = $computer.Description
+    if(!($Description)){$Description = 0}
+    $Aduser = 0
+    $ADuser = get-aduser -Filter {SamAccountName -eq $Description} -EA Ignore
+    if($Aduser){
+        $firstComma = $Computer.DistinguishedName.indexOf(",") + 1
+        $ComputerDistinguishedName = $Computer.DistinguishedName.Substring($firstComma)
+
+        $firstComma = $Aduser.DistinguishedName.IndexOf(",") + 1
+
+        $SecondComma = $Aduser.DistinguishedName.subString($firstComma).IndexOf(",") + 1 + $firstComma
+        $ADUserDistinguishedName = $Aduser.DistinguishedName.subString($SecondComma)
+
+        if($ComputerDistinguishedName -eq $ADUserDistinguishedName){
+            
+            $Result = 'Okay'
+        }
+        Else{
+            
+            $Result = 'Not Okay'
+        }
+
+    }
+    Else{
+        
+        $Result = 'Skipped'
+    }
+
+    $Output += new-object PSobject -Property @{ComputerName = $computer.Name
+    Result = $Result
+    Computerfolder = $ComputerDistinguishedName
+    UserFolder = $ADUserDistinguishedName}
+
+}
+
+$Total = ($Output | ? {$_.Result -ne 'Skipped'}).Count
+$Okay = ($Output | ? {$_.Result -ne 'Okay'}).Count
+$NotOkay = ($Output | ? {$_.Result -ne 'Not Okay'}).Count
+
+write-host (($Okay/$Total)*100)
+
+
+
+
---
+++ IdeaBox/Get-StandardADAccounts.ps1
@@ -0,0 +1,28 @@
+﻿
+#This function will get all the AD users that are useful we can pass a CSV of the properties that we want.
+
+Function Get-StandardADAccounts{
+[cmdletBinding()]
+
+Param(
+    [Parameter(Mandatory = $False)]
+    $ADProperties
+)
+
+Write-Verbose "Properties defined are $ADProperties"
+#The filters are as followed
+#A regular user has an employeeID
+#A contractor does not have an employeeID so we will use Title has the word contractor
+#The User is not disabled (deactivated)
+$ADUsers = Get-ADUser -Filter {((EmployeeID -ne 0) -or (Title -like "*Contractor*")) -and (Enabled -eq $True)} -Properties $ADProperties
+
+Return $ADusers
+
+}
+
+
+$ADProperties = $args[0]
+if(!$ADProperties){
+    $ADProperties = 'Enabled'
+}
+Return Get-StandardADAccounts $ADProperties
