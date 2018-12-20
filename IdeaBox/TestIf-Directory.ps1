5cbe57173b8dd5eb031392afa9c646e48b10852e
NHSD2 <NHSD2@ab.bluecross.ca>
2018-12-20 12:30:44 -0700

    Added

---
+++ IdeaBox/TestIf-Directory.ps1
@@ -0,0 +1,60 @@
+﻿Function TestIf-Directory{
+[cmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    [string]$ComputerName = $env:COMPUTERNAME,
+    [string]$Path
+      
+)
+
+
+$Test = 0
+
+
+#Test = 0 Connection to the computer failed
+#Test = 1 File specified is a directory
+#Test = 2 File specified is a File
+#Test = 3 File Path does not exist
+
+
+
+if(Test-Connection -ComputerName $ComputerName -Count 2 -Quiet){
+    if(Test-Path -Path $Path){
+        $TempLeaf = split-path $Path -Leaf
+        $TempParent = Split-Path $Path -Parent
+        if(Get-ChildItem -Path $TempParent -Filter $TempLeaf | %{$_.PSIsContainer}){
+            #It is a directory
+            $Test = 1 
+        }
+        Else{
+            #it is a file
+            $Test = 2
+        }
+    }
+    Else{
+        #Path does not exist
+        $test = 3
+    }
+}
+Else{
+    #was unable to ping the machine
+    $test = 0
+}
+
+$obj = New-Object PSObject -Property @{
+    ComputerName = $ComputerName
+    Test = $Test
+}
+Write-Verbose ("Completed $Computername with result $test")
+return $obj
+
+}
+
+
+
+$Results = @()
+$Computers = Get-Content $PSScriptRoot\computers.txt
+foreach($computer in $computers){
+    $Path = "\\$Computer\c$\Program Files (x86)\Cisco Finesse"
+    $Results += TestIf-Directory -ComputerName $computer -Path $Path -Verbose
+}
