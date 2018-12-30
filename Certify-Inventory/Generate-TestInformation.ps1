8da17d4cda7c9879b6da1042a04c75963c264467
Nicholas Hoffmann <nrhoffma@ualberta.ca>
2018-12-29 22:32:18 -0700

    Started child script that serves as test data for Certify-Inventory.

--- Certify-Inventory/Generate-TestInformation.ps1
+++ Certify-Inventory/Generate-TestInformation.ps1
@@ -1,1 +1,9 @@
-﻿
+<#
+
+This script will go along Certify-Inventory. It's only purpose is to provide test information so I don't have to query AD and WMI.
+Certify-Inventory will probably be object focused to simplify testing the data.
+
+
+
+
+#>
