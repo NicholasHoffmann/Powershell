bc63213451c8ecdecc581e445fe6cf673584cce1
Nicholas Hoffmann <nrhoffma@ualberta.ca>
2019-01-14 01:36:39 -0700

    Randomness.

--- IdeaBox/Testing-Grounds.ps1
+++ IdeaBox/Testing-Grounds.ps1
@@ -1,4 +1,7 @@
-
+<#
 $Root = Split-Path $PSScriptRoot -Parent
 $Path = Join-Path -Path $Root -ChildPath Modules\WMI-Collection.psm1
 Import-Module -Name $Path
+#>
+
+Measure-Object -Line -InputObject C:\Projects\Powershell\CopyADComputer\Copy-ADComputer.ps1
