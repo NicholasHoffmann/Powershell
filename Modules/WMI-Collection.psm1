522f1df3691d4f744e3db2dc9143084ddb399b63
Nicholas Hoffmann <nrhoffma@ualberta.ca>
2019-01-25 00:26:58 -0700

    Fixed bug where Get-MonitorInformation only queried the host computer.

--- Modules/WMI-Collection.psm1
+++ Modules/WMI-Collection.psm1
@@ -58,7 +58,7 @@ Function Get-MacAddress{
         )
         if(Test-Connection -ComputerName $ComputerName -Count 2 -Quiet){
             Try{
-                $WMIMonitor = Get-WmiObject -Namespace Root\WMI -Query "Select $Properties From WMIMonitorID"
+                $WMIMonitor = Get-WmiObject -Namespace Root\WMI -Query "Select $Properties From WMIMonitorID" -ComputerName $ComputerName
                 $MonitorInformation = @()
                 $Count = 1
                 
@@ -98,3 +98,37 @@ Function Get-MacAddress{
         }
     
     }
+
+    Function Get-PhysicalMemoryCapacity{
+        [cmdletBinding()]
+        Param(
+            [Parameter(Mandatory = $False, ValueFromPipeline = $True)]
+            [object]$ComputerName = $ENV:ComputerName
+    
+        )
+        Begin{
+            $Query = "Select Capacity from Win32_PhysicalMemory"
+            $Namespace = "Root\CimV2"
+        }
+    
+        Process{
+            $Capacity = $Null
+            if(Test-Connection -ComputerName $ComputerName -Count 2 -Quiet){
+                Try{
+                    $Capacity = (Get-WmiObject -ComputerName $ComputerName -Query $Query -Namespace $Namespace | Measure-Object -Property Capacity -Sum).Sum
+                    $Capacity = $Capacity/[Math]::Pow(1024,3)
+                }
+                Catch{
+                    Write-Error -Message "Unable to Query WMI on Computer $ComputerName"
+                }
+            }
+            Else{
+                Write-Error -Message "Unable to ping Computer $ComputerName"
+            }
+        }
+    
+        End{
+            Return $Capacity
+        }
+    }
+    
